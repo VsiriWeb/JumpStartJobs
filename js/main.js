f1fb02d6177efbee26765fc5136a38774247acdb
Vyom Siriyapu <156244677+VsiriWeb@users.noreply.github.com>
2025-04-06 19:27:50 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,6 +40,19 @@
     });
 
 
+//email
+    function sendMail(){
+        let params = {
+            name : document.getElementById("name").value,
+            email : document.getElementById("email").value,
+            subject : document.getElementById("subject").value,
+            message : document.getElementById("message").value,
+
+        }
+        emailjs.send("service_JumpStartJobs", "template_zpdm3q7",params).then(alert("email sent!")) 
+    }
+
+    
     // Header carousel
     $(".header-carousel").owlCarousel({
         autoplay: true,
